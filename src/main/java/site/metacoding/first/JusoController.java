c07f40a88b1e5890f666dadef6731a34821794d8
jinyongkwon <wlsdyd1178@naver.com>
2022-05-23 12:21:50 +0900

    주소 기초세팅완료

---
+++ src/main/java/site/metacoding/first/JusoController.java
@@ -0,0 +1,13 @@
+package site.metacoding.first;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JusoController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
