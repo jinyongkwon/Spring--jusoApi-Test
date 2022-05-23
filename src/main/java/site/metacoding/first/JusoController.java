b0569726845a71f1989222abdeaf0db93ec1a14e
jinyongkwon <wlsdyd1178@naver.com>
2022-05-23 14:08:39 +0900

    주소 찾기 - 베이직 로직

--- src/main/java/site/metacoding/first/JusoController.java
+++ src/main/java/site/metacoding/first/JusoController.java
@@ -1,7 +1,11 @@
 package site.metacoding.first;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class JusoController {
@@ -9,5 +13,31 @@ public class JusoController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @PostMapping("/juso/callback")
+    public void jusoCallback(String roadFullAddr) {
+        System.out.println(roadFullAddr);
+    }
+
+    @RequestMapping(value = "/juso/popup", method = { RequestMethod.GET, RequestMethod.POST })
+    public String jusoPopup(Model model, String roadFullAddr) {
+
+        String jusoUrl = "https://www.juso.go.kr/addrlink/addrLinkUrl.do";
+        String confmKey = "devU01TX0FVVEgyMDIyMDUyMzEwNDgzNTExMjYwMDA=";
+        String returnUrl = "http://localhost:8080/juso/popup";
+        String resultType = "4";
+
+        if (roadFullAddr == null) {
+            roadFullAddr = "N";
+        }
+
+        model.addAttribute("jusoUrl", jusoUrl);
+        model.addAttribute("confmKey", confmKey);
+        model.addAttribute("returnUrl", returnUrl);
+        model.addAttribute("resultType", resultType);
+        model.addAttribute("roadFullAddr", roadFullAddr);
+
+        return "jusoPopup";
     }
 }
